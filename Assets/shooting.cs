9ad9e64a55a88d96f4e6d7ae1437daa571b73388
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-04-09 04:14:16 +0700

    add unity project

---
+++ Assets/shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject instBullet = Instantiate(bullet,transform.transform.position,Quaternion.identity) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(Vector3.forward*speed);
+            Destroy(instBullet, 3f);
+        }
+         
+    }
+}
---
+++ Assets/tankcontroll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankcontroll : MonoBehaviour
+{
+   public float speed;
+   public float rotationspeed;
+
+   private void Update() 
+   {
+      if(Input.GetKey(KeyCode.W))  
+      {
+         transform.Translate(Vector3.forward* Time.deltaTime*speed,Space.Self);
+         
+      }
+       if(Input.GetKey(KeyCode.S))  
+      {
+         transform.Translate(Vector3.back* Time.deltaTime*speed,Space.Self);
+      }
+      if(Input.GetKey(KeyCode.A))  
+      {
+         transform.Rotate(Vector3.up* Time.deltaTime*rotationspeed);
+      }
+       if(Input.GetKey(KeyCode.D))  
+      {
+         transform.Rotate(Vector3.down* Time.deltaTime*rotationspeed);
+      }
+   }
+
+
+}
